18e0e5b1dda25aa0128857ef05567d6a8bdba8a2
yaojinpeng <yaojinpeng@wq.com>
2022-04-13 20:10:32 +0800

    master-01 first commit 第一次提交

--- cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/Controller/OrderController.java
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/Controller/OrderController.java
@@ -21,6 +21,7 @@ public class OrderController {
     @RequestMapping("/consumer/consul")
     public String consumerConsul(){
         System.out.println("我是服务调用者");
+        System.out.println("master-01-------->   我是微服务调用者");
         return restTemplate.getForObject(CONSUL_URL+"/payment/consul",String.class);
     }
 }
